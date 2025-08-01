39e555468d1ba5c4c6cd94c2a8d22796c7fb6ec2
AntonioF <antonioalbertoforchi@gmail.com>
2025-08-01 20:33:58 +0200

    json validator fully functional

--- src/app/(pages)/json-validator/page.tsx
+++ src/app/(pages)/json-validator/page.tsx
@@ -1,7 +1,64 @@
+"use client";
+
 import React from "react";
-import { Code, AlertCircle } from "lucide-react";
+import { useState } from "react";
+import { Code, AlertCircle, CircleCheckBig, Copy } from "lucide-react";
+import { formatJSON, isValidJSON } from "@/lib/jsonValidate";
+import { Bounce, toast } from "react-toastify";
 
 const JsonValidator = () => {
+  const [jsonInput, setJsonInput] = useState("");
+  const [validationResult, setValidationResult] = useState(false);
+  const [formattedJson, setFormattedJson] = useState("");
+  const [validationStarted, setValidationStarted] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleValidate = () => {
+    try { 
+      if (!jsonInput) {
+        setFormattedJson('');
+        setValidationStarted(false);
+        setValidationResult(false);
+
+        toast.error("Please enter valid JSON data", {
+          autoClose: 3000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "dark",
+          transition: Bounce,
+        });
+        return;
+      }
+
+      setValidationStarted(true);
+
+      if (isValidJSON(jsonInput).valid) {
+        setValidationResult(true);
+        const formatted = formatJSON(jsonInput);
+        setFormattedJson(formatted);
+      }else{
+        setValidationResult(false);
+        setErrorMessage(isValidJSON(jsonInput).error || "");
+        setFormattedJson('');
+      }
+
+    } catch (error) {
+      toast.error("Invalid JSON format. Please check your input", {
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+        transition: Bounce,
+      });
+    }
+  };
+
   return (
     <>
       <div className="flex flex-col items-center justify-center min-h-screen w-screen text-white px-8 mb-4">
@@ -24,22 +81,69 @@ const JsonValidator = () => {
             <textarea
               name=""
               id=""
+              value={jsonInput}
+              onChange={(e) => setJsonInput(e.target.value)}
               placeholder='{"name": "John Doe", "email": "john@example.com", "age": 30}'
               className="bg-white/3 backdrop-blur-xl border border-white/10 py-2 px-4 rounded-xl h-96"
             ></textarea>
-            <button className="w-full my-2 bg-gradient-to-l from-purple-600 via-pink-400 to-purple-600 text-black rounded-lg p-2 flex gap-2 items-center justify-center font-semibold group-button shadow-black/60 shadow-lg hover:shadow-xl">
+            <button
+              onClick={handleValidate}
+              className="w-full my-2 bg-gradient-to-l from-purple-600 via-pink-400 to-purple-600 text-black rounded-lg p-2 flex gap-2 items-center justify-center font-semibold group-button shadow-black/60 shadow-lg hover:shadow-xl"
+            >
               Validate JSON
             </button>
           </div>
 
-          <div className="w-full h-full bg-white/1 backdrop-blur-xl border transition duration-300 border-white/10 py-4 px-8 rounded-xl shadow-lg shadow-black/60 flex flex-col gap-2">
+          <div className="w-full h-full bg-white/1 backdrop-blur-xl border transition duration-300 border-white/10 py-4 px-8 rounded-xl shadow-lg shadow-black/60 flex flex-col gap-2 relative">
+            <div className="hidden absolute top-4 right-4 laptop:top-6 laptop:right-6 z-10 border p-2 rounded-lg bg-white/1 backdrop-blur-xl hover:bg-white/10 transition duration-300 cursor-pointer">
+              <Copy className="w-5 h-5 text-white/60 hover:text-white" />
+            </div>
+
             <div className="flex items-center gap-2">
-              <AlertCircle className="w-5 h-5 text-red-400" />
+              {validationResult ? (
+                <CircleCheckBig className="w-5 h-5 text-green-400" />
+              ) : (
+                <AlertCircle className="w-5 h-5 text-red-400" />
+              )}
               <span className="text-2xl font-semibold">Validation Result</span>
             </div>
-            <p className="text-gray-400">
+            <p className="text-gray-400 mb-4">
               Validation status and formatted output
             </p>
+
+            <div className={`${validationStarted ? "" : "hidden"}`}>
+              <div
+                className={`w-full h-12 flex gap-2 items-center border ${
+                  validationResult
+                    ? "border-green-400 bg-green-400/20"
+                    : "border-red-400 bg-red-400/20"
+                } rounded-lg p-4`}
+              >
+                {validationResult ? (
+                  <CircleCheckBig className="w-5 h-5 text-green-400" />
+                ) : (
+                  <AlertCircle className="w-5 h-5 text-red-400" />
+                )}
+                <p
+                  className={` ${
+                    validationResult ? "text-green-400" : "text-red-400"
+                  }`}
+                >
+                  {validationResult
+                    ? "Valid JSON! Your JSON has been formatted below."
+                    : `Invalid JSON format. ${errorMessage}`}
+                </p>
+              </div>
+              <div className={`${validationResult ? "" : "hidden"}`}>
+                <p className="text-gray-400 my-2">Formatted JSON:</p>
+                <textarea
+                  name=""
+                  id=""
+                  value={formattedJson}
+                  className="bg-white/3 backdrop-blur-xl border border-white/10 py-2 px-4 rounded-xl max-h-82 h-82 w-full"
+                ></textarea>
+              </div>
+            </div>
           </div>
         </div>
       </div>
